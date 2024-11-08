493dc062c50208a07e723c8a37f9f367289f2506
ethan <xethts@gmail.com>
2024-11-08 12:01:00 -0500

    users APIs urls fixed

--- server/users/urls.py
+++ server/users/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 # Add URLs here when you create new APIs in this app
 urlpatterns = [
-    path("login_auth_example", views.login_auth_example, name="user"),
+    # path("login_auth_example", views.login_auth_example, name="user"),
+    path("register", views.register_user, name="register"),
+    path("login", views.login_user, name="login"),
+    path("protected_route", views.protected_route, name="protected_route"),
+
 ]
