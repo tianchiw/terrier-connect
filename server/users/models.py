e982a352b67f685e85b887c64f423e3d44c5a7a5
Damodhar Pai <damodhar@bu.edu>
2024-11-07 23:32:49 -0500

    Added Users model

--- server/users/models.py
+++ server/users/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
+
+# User model
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    display_name = models.CharField(max_length=100, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    avatar_url = models.URLField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    #objects = UserManager()
+
+    #USERNAME_FIELD = 'email'
+    #REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
