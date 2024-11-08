6a2eb4ecd7f659e29b404090b6ef3c0c83389b3c
damodharpai <damodhar@bu.edu>
2024-11-08 01:05:47 -0500

    Added User and Users Follow Rel Model

--- server/users/models.py
+++ server/users/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
 from django.utils import timezone
 
-# User model
 class User(models.Model):
     email = models.EmailField(unique=True)
     display_name = models.CharField(max_length=100, blank=True, null=True)
@@ -11,11 +10,23 @@ class User(models.Model):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    password = models.CharField(max_length=128)  # Stores hashed password
 
-    #objects = UserManager()
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+        self.save()
 
-    #USERNAME_FIELD = 'email'
-    #REQUIRED_FIELDS = []
+    def check_password(self, raw_password):
+        from django.contrib.auth.hashers import check_password
+        return check_password(raw_password, self.password)
 
     def __str__(self):
         return self.email
+
+class UserFollowRel(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.follower.display_name} follows {self.following.display_name}"
