add945de157414de310b9b16fb06ecfea3e3668f
unknown <gr4113@bu.edu>
2024-11-15 07:53:29 +0800

    Rephase the structures and add jumpto function to header buttons

--- clienthomepage/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import TabReducer from './reducers/tab'
-
-export default configureStore({
-    reducer: {
-        tab: TabReducer
-    }
-})
--- server/hashtags/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Hashtag, PostHashtagRel
-
-# Register your models here.
-admin.site.register(Hashtag)
-admin.site.register(PostHashtagRel)
