1333f3d2f737c422bde4601298faceeeb5cfe745
Damodhar Pai <damodhar@bu.edu>
2024-11-17 13:44:37 -0500

    Update urls.py with the APIs for follow, unfollow and list followers

--- server/users/urls.py
+++ server/users/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("register", views.register_user, name="register"),
     path("login", views.login_user, name="login"),
     path("protected_route", views.protected_route, name="protected_route"),
+    path('user/<int:user_id>/', views.get_user_info_by_id, name='get_user_info_by_id'),
+    path('<int:user_id>/follow/', views.follow_user, name='follow_user'),
+    path('<int:user_id>/unfollow/', views.unfollow_user, name='unfollow_user'),
+    path('<int:user_id>/followers/', views.list_followers, name='list_followers'),
 
 ]
