5a9bac508645041718c8ef38a0b6766281308191
Damodhar Pai <damodhar@bu.edu>
2024-11-18 23:12:15 -0500

    Updated the urls for the new APIs

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from . import views
 
-# Add URLs here when you create new APIs in this app
+# Define user-related API endpoints
 urlpatterns = [
-    # path("login_auth_example", views.login_auth_example, name="user"),
     path("register", views.register_user, name="register"),
     path("login", views.login_user, name="login"),
     path("protected_route", views.protected_route, name="protected_route"),
@@ -12,5 +10,5 @@ urlpatterns = [
     path('<int:user_id>/follow/', views.follow_user, name='follow_user'),
     path('<int:user_id>/unfollow/', views.unfollow_user, name='unfollow_user'),
     path('<int:user_id>/followers/', views.list_followers, name='list_followers'),
-
+    path('<int:user_id>/following/', views.list_following, name='list_following'),  # Added for listing following users
 ]
