5e57a681a65c0bc148a4b2710656efa71e7cf586
grfeng4113 <gr4113@bu.edu>
2024-12-09 04:11:48 +0800

    fix some bugs

--- client/src/pages/forumPost/postStyles.js
+++ client/src/pages/forumPost/postStyles.js
@@ -5,3 +5,15 @@ export const postBoxStyle = {
     borderRadius: 4,
     marginTop: 4,
   };
+
+  export const commentStyle ={
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    borderRadius: 2,
+    p: 4,
+  };
